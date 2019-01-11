cf486408b9000d0cfc0cdc15fae60b626e6717a8
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2019-01-11 17:49:45 +0900

    게시판 글 등록/리스트 조회2

--- routes/inc.route.js
+++ routes/inc.route.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const board_controller = require('../controllers/board.controller');
 const user_controller = require('../controllers/user.controller');
 
 router.get('/test', user_controller.test);
 router.post('/user', user_controller.join);
 router.get('/user', user_controller.list);
 router.post('/approve', user_controller.approve);
+router.post('/board', board_controller.join);
+router.get('/board', board_controller.list);
 
 module.exports = router;
